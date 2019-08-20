5cd061f5fb06fa8d1f6d54b25d68eb25b3a36db6
Ratnadeep <govanderatnadeep@gmail.com>
2019-08-20 23:55:13 +0530

    Initial Commit

---
+++ MacOSDemo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MacOSDemo
+//
+//  Created by Ratnadeep on 8/20/19.
+//  Copyright © 2019 RatnaDeep. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MacOSDemo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MacOSDemo
+//
+//  Created by Ratnadeep on 8/20/19.
+//  Copyright © 2019 RatnaDeep. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
